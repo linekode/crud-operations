8f93e0b54c4a35e9cd97a614a7656bec24fde891
Alpha Diop <alphakhoss@gmail.com>
2025-02-04 17:29:36 +0000

    FEAT: ✨ Add delete feature

--- delete.php
+++ delete.php
@@ -1,3 +1,11 @@
 <?php
     require 'db.php';
+    $id = $_GET['id'];
+    
+    $stmt = $pdo->prepare("DELETE FROM users where id= :id");
+    $stmt->bindParam(':id', $id);
+    $stmt->execute();
+
+    header('Location: index.php');
+    exit();
 ?>
